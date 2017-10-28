facebookarchive/caffe2

commit 8539a1e78b7fd8aa92b08a8f24b2b0f7978bf5a5

Add tensor output that gives the sampled values
Summary: Given an additional tensor containing the values corresponding to the weighted samples, add tensor output that contains the values selected by the sampled indexes.

Reviewed By: akyrola

Differential Revision: D6050094

fbshipit-source-id: 1eccc641b99e30d36ae83d49f630b018a53e4147

caffe2/operators/weighted_sample_op.cc
@@ -4,38 +4,66 @@ namespace caffe2 {
 
 template <>
 bool WeightedSampleOp<float, CPUContext>::RunOnDevice() {
+  CAFFE_ENFORCE_EQ(
+      InputSize(),
+      OutputSize(),
+      "The number of tensors of the input and the output must be the same.");
   auto& weights = Input(0);
   int batch_size = weights.dim(0);
   int weights_dim = weights.dim(1);
-  auto* output = Output(0);
+  auto* out_idx = Output(0);
 
   if (batch_size > 0 && weights_dim > 0) {
-    output->Resize(batch_size, 1);
     cum_mass_.resize(weights_dim);
     const float* mat_weights = weights.template data<float>();
-    int* output_indices = output->template mutable_data<int>();
+    const float* mat_values = nullptr;
+    out_idx->Resize(batch_size, 1);
+    int* output_indices = out_idx->template mutable_data<int>();
+    float* output_values = nullptr;
+
+    if (InputSize() == 2) {
+      auto& values = Input(1);
+      CAFFE_ENFORCE_EQ(
+          weights.dims(),
+          values.dims(),
+          "The sampling weights tensor and the sampling values tensor must have the same dimensions.");
+      mat_values = values.template data<float>();
+      auto* out_value = Output(1);
+      out_value->Resize(batch_size, 1);
+      output_values = out_value->template mutable_data<float>();
+    }
 
     for (int i = 0; i < batch_size; i++) {
-      offset_ = i * weights_dim;
+      float r;
+      int offset = i * weights_dim;
 
-      cum_mass_[0] = mat_weights[offset_];
+      cum_mass_[0] = mat_weights[offset];
       for (int j = 1; j < weights_dim; j++) {
-        cum_mass_[j] = cum_mass_[j - 1] + mat_weights[offset_ + j];
+        cum_mass_[j] = cum_mass_[j - 1] + mat_weights[offset + j];
       }
 
       math::RandUniform<float, CPUContext>(
-          1, 0.0f, cum_mass_[cum_mass_.size() - 1], &r_, &context_);
+          1, 0.0f, cum_mass_[cum_mass_.size() - 1], &r, &context_);
       // Makes the element in cum_mass_ slightly bigger
       // to compensate inaccuracy introduced due to rounding,
       cum_mass_[cum_mass_.size() - 1] += 0.01f;
-      auto lb = lower_bound(cum_mass_.begin(), cum_mass_.end(), r_);
-      CAFFE_ENFORCE(
-          lb != cum_mass_.end(), "Cannot find ", r_, " in cum_mass_.");
+      auto lb = lower_bound(cum_mass_.begin(), cum_mass_.end(), r);
+      CAFFE_ENFORCE(lb != cum_mass_.end(), "Cannot find ", r, " in cum_mass_.");
       output_indices[i] = static_cast<int>(lb - cum_mass_.begin());
+
+      if (output_values) {
+        output_values[i] =
+            static_cast<float>(mat_values[offset + (lb - cum_mass_.begin())]);
+      }
     }
   } else {
-    output->Resize(0);
-    output->template mutable_data<int>();
+    out_idx->Resize(0);
+    out_idx->template mutable_data<int>();
+    if (OutputSize() == 2) {
+      auto* out_value = Output(1);
+      out_value->Resize(0);
+      out_value->template mutable_data<float>();
+    }
   }
 
   return true;
@@ -44,35 +72,48 @@ bool WeightedSampleOp<float, CPUContext>::RunOnDevice() {
 REGISTER_CPU_OPERATOR(WeightedSample, WeightedSampleOp<float, CPUContext>);
 
 OPERATOR_SCHEMA(WeightedSample)
-    .NumInputs(1)
-    .NumOutputs(1)
+    .NumInputs(1, 2)
+    .NumOutputs(1, 2)
     .TensorInferenceFunction([](const OperatorDef& def,
                                 const vector<TensorShape>& in) {
-      vector<TensorShape> out(1);
+      vector<TensorShape> out(2);
       int batch_size = in[0].dims(0);
       out[0] = CreateTensorShape(vector<int>{batch_size}, TensorProto::INT32);
+      out[1] = CreateTensorShape(vector<float>{batch_size}, TensorProto::FLOAT);
       return out;
     })
     .SetDoc(R"DOC(
 
-    The operator performs sampling based on the input sampling weights for each batch.
-    All weights must be non-negative numbers.
+    The operator performs sampling based on the input sampling weights for
+    each batch. All weights must be non-negative numbers.
     The input is a 2-D tensor (Tensor<float>) of size (batch_size x weights_dim).
-    For each batch, an index is randomly sampled from the distribution given by the weights of the corresponding batch.
-    The output is a 1-D tensor (Tensor<int>) of size (batch_size x 1) and contains the index(es) of the sampled output.
-
+    For each batch, an index is randomly sampled from the distribution given by
+    the weights of the corresponding batch.
+    The output is a 1-D tensor (Tensor<int>) of size (batch_size x 1) and
+    contains the index(es) of the sampled output.
 
     )DOC")
     .Input(
         0,
         "sampling_weights",
-        "The input is a 2-D tensor (Tensor<float>) of size (batch_size x weights_dim)."
+        "A 2-D Tensor<float> of size (batch_size x weights_dim)."
         "All weights must be non-negative numbers.")
+    .Input(
+        1,
+        "sampling_values",
+        "An optional 2-D Tensor<float> of size (batch_size x weights_dim)."
+        "Its values correspond to the sampling weights.")
     .Output(
         0,
         "sampled_indexes",
-        "The output tensor contains index(es) sampled from distribution given by the weight vector(s) in the input tensor"
-        "The output is a 1-D tensor (Tensor<int>) of size (batch_size x 1)");
+        "The output tensor contains index(es) sampled from distribution given"
+        "by the weight vector(s) in the input tensor"
+        "The output is a 1-D Tensor<int> of size (batch_size x 1)")
+    .Output(
+        1,
+        "sampled_values",
+        "The output tensor contains value(s) selected by the sampled index(es)"
+        "It is a 1-D Tensor<float> of size (batch_size x 1)");
 
 SHOULD_NOT_DO_GRADIENT(WeightedSample);
 } // namespace caffe2

caffe2/operators/weighted_sample_op.h
@@ -22,8 +22,6 @@ class WeightedSampleOp final : public Operator<Context> {
 
  private:
   vector<float> cum_mass_;
-  float r_;
-  int offset_;
   Tensor<Context> unif_samples_;
 };
 

caffe2/python/operator_test/weighted_sample_test.py
@@ -20,24 +20,45 @@ class TestWeightedSample(hu.HypothesisTestCase):
         **hu.gcs
     )
     def test_weighted_sample(self, batch, weights_len, gc, dc):
-        op = core.CreateOperator(
-            "WeightedSample",
-            ["weights"],
-            ["indices"],
-        )
 
         weights = np.zeros((batch, weights_len))
+        values = np.zeros((batch, weights_len))
         rand_indices = []
+        rand_values = []
         if batch > 0 and weights_len > 0:
             for i in range(batch):
                 rand_tmp = np.random.randint(0, weights_len)
+                rand_val = np.random.rand()
                 rand_indices.append(rand_tmp)
+                rand_values.append(rand_val)
                 weights[i, rand_tmp] = 1.0
+                values[i, rand_tmp] = rand_val
 
         rand_indices = np.array(rand_indices, dtype=np.float32)
+        rand_values = np.array(rand_values, dtype=np.float32)
         workspace.FeedBlob("weights", weights.astype(np.float32))
+        workspace.FeedBlob("values", values.astype(np.float32))
+        op = core.CreateOperator(
+            "WeightedSample", ["weights", "values"],
+            ["sample_indices", "sample_values"]
+        )
         workspace.RunOperatorOnce(op)
-        result = workspace.FetchBlob("indices")
+        result_indices = workspace.FetchBlob("sample_indices")
+        result_values = workspace.FetchBlob("sample_values")
+
+        if batch > 0 and weights_len > 0:
+            for i in range(batch):
+                np.testing.assert_allclose(rand_indices[i], result_indices[i])
+                np.testing.assert_allclose(rand_values[i], result_values[i])
+        else:
+            np.testing.assert_allclose(rand_indices, result_indices)
+            np.testing.assert_allclose(rand_values, result_values)
+
+        op2 = core.CreateOperator(
+            "WeightedSample", ["weights"], ["sample_indices_2"]
+        )
+        workspace.RunOperatorOnce(op2)
+        result = workspace.FetchBlob("sample_indices_2")
 
         if batch > 0 and weights_len > 0:
             for i in range(batch):
@@ -45,7 +66,9 @@ class TestWeightedSample(hu.HypothesisTestCase):
         else:
             np.testing.assert_allclose(rand_indices, result)
 
-        self.assertDeviceChecks(dc, op, [weights.astype(np.float32)], [0])
+        # Add this back after CUDA operator is updated
+        # self.assertDeviceChecks(dc, op, [weights.astype(np.float32)], [0])
+
 
 if __name__ == "__main__":
     import unittest

kavlal6/pytorch

commit 11a155e54acb72c943ac29f781ab6189529eacb8

Fix bug introduced in a recent commit
Summary:
This is introduced in https://github.com/caffe2/caffe2/commit/8539a1e78b7fd8aa92b08a8f24b2b0f7978bf5a5 - vector<float> should not be used in Tensor shape inference.
Closes https://github.com/caffe2/caffe2/pull/1393

Reviewed By: akyrola

Differential Revision: D6181075

Pulled By: Yangqing

fbshipit-source-id: 002144a137148b5b16118d0c123132890e8d325a

caffe2/operators/weighted_sample_op.cc
@@ -79,7 +79,7 @@ OPERATOR_SCHEMA(WeightedSample)
       vector<TensorShape> out(2);
       int batch_size = in[0].dims(0);
       out[0] = CreateTensorShape(vector<int>{batch_size}, TensorProto::INT32);
-      out[1] = CreateTensorShape(vector<float>{batch_size}, TensorProto::FLOAT);
+      out[1] = CreateTensorShape(vector<int>{batch_size}, TensorProto::FLOAT);
       return out;
     })
     .SetDoc(R"DOC(
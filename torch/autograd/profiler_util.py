kavlal6/pytorch

commit 88ab3e43228b7440a33bf534cde493446a31538c

[BE]: Update ruff to 0.285 (#107519)
This updates ruff to 0.285 which is faster, better, and have fixes a bunch of false negatives with regards to fstrings.

I also enabled RUF017 which looks for accidental quadratic list summation. Luckily, seems like there are no instances of it in our codebase, so enabling it so that it stays like that. :)

Pull Request resolved: https://github.com/pytorch/pytorch/pull/107519
Approved by: https://github.com/ezyang

torch/autograd/profiler_util.py
@@ -246,20 +246,14 @@ class EventList(list):
                     # 's' and 'f' draw Flow arrows from
                     # the CPU launch to the GPU kernel
                     f.write(
-                        '{{"name": "{}", '
+                        f'{{"name": "{evt.trace_name}", '
                         '"ph": "s", '
-                        '"ts": {}, '
-                        '"tid": {}, '
+                        f'"ts": {evt.time_range.start}, '
+                        f'"tid": {evt.thread}, '
                         '"pid": "CPU functions", '
-                        '"id": {}, '
-                        '"cat": "cpu_to_{}", '
-                        '"args": {{}}}}, '.format(
-                            evt.trace_name,
-                            evt.time_range.start,
-                            evt.thread,
-                            next_id,
-                            device_name,
-                        )
+                        f'"id": {next_id}, '
+                        f'"cat": "cpu_to_{device_name}", '
+                        '"args": {{}}}}, '
                     )
                     # Note: use torch.profiler to get device kernel trace
                     next_id += 1

commit 2bbc8fc536762c39914291f397b53708975b940d

Fix chrome trace entry format (#113763)
Fix regression introduced by https://github.com/pytorch/pytorch/pull/107519

`'"args": {{}}}}, '` was part of format string, when curly braces a duplicated to get them printed single time, but ruff change left the string format as is

Fixes https://github.com/pytorch/pytorch/issues/113756

Pull Request resolved: https://github.com/pytorch/pytorch/pull/113763
Approved by: https://github.com/Skylion007, https://github.com/aaronenyeshi

torch/autograd/profiler_util.py
@@ -253,7 +253,7 @@ class EventList(list):
                         '"pid": "CPU functions", '
                         f'"id": {next_id}, '
                         f'"cat": "cpu_to_{device_name}", '
-                        '"args": {{}}}}, '
+                        '"args": {}}, '
                     )
                     # Note: use torch.profiler to get device kernel trace
                     next_id += 1